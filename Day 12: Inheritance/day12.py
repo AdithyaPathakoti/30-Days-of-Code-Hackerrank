1e8462c4b1eeafe8c52d0911281241aa5ae62438
Adithya Pathakoti <151919367+AdithyaPathakoti@users.noreply.github.com>
2024-08-29 21:05:34 +0530

    Create day12.py

---
+++ Day 12: Inheritance/day12.py
@@ -0,0 +1,58 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+        def __init__(self, firstName, lastName, idNumber,scores):
+            self.firstName = firstName
+            self.lastName = lastName
+            self.idNumber = idNumber
+            self.scores = scores
+        def calculate(self):
+            sum=0
+            for scores in self.scores:
+                sum=sum+scores
+            avg=sum/len(self.scores)
+            if 90<=avg and avg <=100:
+                return 'O'
+            elif 80<=avg and avg <90:
+                return 'E'
+            elif 70<=avg and avg <80:
+                return 'A'
+            elif 55<=avg and avg <70:
+                return 'P'
+            elif 40<=avg and avg <55:
+                return 'D'
+            else:
+                return 'T'
+  
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
